a16165cf5f669f6bb2ace0e5ada976e4d922905f
Abel Tran <abeltran.develop@gmail.com>
2023-06-02 17:12:48 +0700

    fix: update keymaps

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -13,11 +13,11 @@ require("gitsigns").setup {
     buffer = true,
     ["n ]c"] = {expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''},
     ["n [c"] = {expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''},
-    ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
+    ["n <leader>gs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ["n <leader>gu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ["n <leader>gr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ["n <leader>gp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ["n <leader>gb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
   },
   watch_gitdir = {
     interval = 100
